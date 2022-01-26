1318309db094a9dd1467c317fe4f9b684fcd80ca
DmytroMS <78643116+DmytroMS@users.noreply.github.com>
2022-01-26 09:19:38 +0200

    done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,7 @@ const {joiRegisterShema, joiLoginShema} = require("../../model/user");
 const {BadRequest, Conflict, Unauthorized} = require("http-errors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 
 
 const {SECRET_KEY} = process.env;
@@ -24,7 +25,9 @@ router.post('/signup', async(req, res, next) => {
         const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password,salt);
 
-        const newUser = await User.create({email, password: hashPassword});
+        const avatarURL = gravatar.url(email);
+
+        const newUser = await User.create({email, password: hashPassword, avatarURL});
         res.status(201).json({
             user: {
                 email: newUser.email,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const {User} = require("../../model");
 const router = express.Router();
-const {authenticate} = require("../../middlewares");
+const {authenticate, upload} = require("../../middlewares");
+const path = require("path");
+const fs = require("fs/promises");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 router.get("/current", authenticate, async(req, res) => {
     const {email, subscription} = req.user;
@@ -15,6 +19,18 @@ router.get("/logout", authenticate, async(req, res) => {
     const {_id} = req.user; 
     await User.findByIdAndUpdate(_id, {token: null});
     res.status(204).send();
+});
+
+router.patch("/avatars", authenticate, upload.single("avatar"), async(req, res) => {
+  const {path: tempUpload, filename} = req.file;
+  const [extension] = filename.split(".").reverse();
+  const newFleName = `${req.user._id}.${extension}`;  // создаем имя загружаемого аватара, ид пользователя + расширение файла 
+  const fileUpload = path.join(avatarsDir, newFleName); 
+  await fs.rename(tempUpload, fileUpload); // перемещаем аватар с папки темповой в паблик
+  const avatarURL = path.join("avatars", newFleName);
+  await User.findByIdAndUpdate(req.user._id, {avatarURL}, {new: true}); 
+  res.json({avatarURL})
 })
 
 module.exports = router;
+
